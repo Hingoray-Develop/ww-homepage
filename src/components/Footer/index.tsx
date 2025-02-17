3db09261a5c6fd95eef25e8f22637cbb3c149133
Jiam <53891443+blood8879@users.noreply.github.com>
2025-02-17 16:28:28 +0900

    feat: update Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Body3, Frame, FrameScreen, Heading6, Image, Text } from "@/atoms";
+import { Frame, FrameScreen, Image, Text } from "@/atoms";
 import { colors } from "@/styles";
 import Button from "../Button";
 import Divider from "../Divider";
@@ -10,24 +10,53 @@ import ArrowRight from "@/assets/icons/line/arrow-right.svg?react";
 const Footer = () => {
   return (
     <FrameScreen overflow="hidden">
-      <Frame w="100%" col alignment="center" bg={colors.neutral[950]} p={40}>
-        <Frame w="100%" col p={40} bg={colors.neutral[800]} row radius={32}>
+      <Frame
+        w="100%"
+        col
+        alignment="center"
+        bg={colors.neutral[950]}
+        p={40}
+        mobile={{ p: 20 }}
+        tablet={{ p: 30 }}
+      >
+        <Frame
+          w="100%"
+          col
+          p={40}
+          bg={colors.neutral[800]}
+          row
+          radius={32}
+          mobile={{ p: 20 }}
+          tablet={{ p: 30 }}
+        >
           <Frame w="100%" col>
-            <Frame w="100%" row alignment="top" gap={"auto"} pb={32}>
-              <Frame col>
+            <Frame
+              w="100%"
+              row
+              alignment="center"
+              gap="auto"
+              pb={32}
+              // mobile={{ pb: 20 }}
+              // tablet={{ pb: 24 }}
+            >
+              <Frame col alignment="center">
                 <Text
                   fontSize={52}
-                  lineHeight={"72px"}
+                  lineHeight="72px"
                   fontWeight={700}
                   fontColor={colors.white}
+                  mobile={{ fontSize: 28, lineHeight: "36px" }}
+                  tablet={{ fontSize: 36, lineHeight: "48px" }}
                 >
                   IT 솔루션 개발,
                 </Text>
                 <Text
                   fontSize={52}
-                  lineHeight={"72px"}
+                  lineHeight="72px"
                   fontWeight={700}
                   fontColor={colors.main[200]}
+                  mobile={{ fontSize: 28, lineHeight: "36px" }}
+                  tablet={{ fontSize: 36, lineHeight: "48px" }}
                 >
                   첫삽부터 제대로
                 </Text>
@@ -40,33 +69,105 @@ const Footer = () => {
                     console.log("빠른 프로젝트 견적내기");
                   }}
                 >
-                  <Frame row alignment="center" px={40} py={18} gap={8}>
-                    <Heading6 fontColor={colors.white} fontWeight={700} py={2}>
+                  <Frame
+                    row
+                    alignment="center"
+                    px={40}
+                    py={18}
+                    gap={8}
+                    mobile={{ px: 20, py: 12 }}
+                    tablet={{ px: 30, py: 14 }}
+                  >
+                    <Text
+                      fontColor={colors.white}
+                      fontWeight={700}
+                      py={2}
+                      fontSize={18}
+                      mobile={{ fontSize: 16 }}
+                      tablet={{ fontSize: 18 }}
+                    >
                       빠른 프로젝트 견적내기
-                    </Heading6>
+                    </Text>
                     <ArrowRight width={24} height={24} fill={colors.white} />
                   </Frame>
                 </Button>
               </Frame>
             </Frame>
             <Divider color={colors.neutral[700]} thickness={1} />
-            <Frame w="100%" pt={32} row gap={"auto"} alignment="center">
-              <Frame>
-                <Frame w="100%" row gap={8}>
-                  <Body3 fontColor={colors.neutral[400]}>흰고래컴퍼니</Body3>
-                  <Body3 fontColor={colors.neutral[400]}>|</Body3>
-                  <Body3 fontColor={colors.neutral[400]}>대표 마성준</Body3>
-                  <Body3 fontColor={colors.neutral[400]}>|</Body3>
-                  <Body3 fontColor={colors.neutral[400]}>
+            <Frame
+              w="100%"
+              pt={32}
+              row
+              gap="auto"
+              alignment="center"
+              mobile={{ pt: 20 }}
+              tablet={{ pt: 24 }}
+            >
+              <Frame mobile={{ pb: 16 }}>
+                <Frame
+                  w="100%"
+                  row
+                  gap={8}
+                  mobile={{ alignment: "center" }}
+                  tablet={{ row: true, alignment: "center" }}
+                >
+                  <Text
+                    fontSize={14}
+                    lineHeight={"22px"}
+                    mobile={{ fontSize: 12, lineHeight: "18px" }}
+                    fontColor={colors.neutral[400]}
+                  >
+                    흰고래컴퍼니
+                  </Text>
+                  <Text
+                    fontSize={14}
+                    lineHeight={"22px"}
+                    mobile={{ fontSize: 12, lineHeight: "18px" }}
+                    fontColor={colors.neutral[400]}
+                  >
+                    |
+                  </Text>
+                  <Text
+                    fontSize={14}
+                    lineHeight={"22px"}
+                    mobile={{ fontSize: 12, lineHeight: "18px" }}
+                    fontColor={colors.neutral[400]}
+                  >
+                    대표 마성준
+                  </Text>
+                  <Text
+                    fontSize={14}
+                    lineHeight={"22px"}
+                    mobile={{ fontSize: 12, lineHeight: "18px" }}
+                    fontColor={colors.neutral[400]}
+                  >
+                    |
+                  </Text>
+                  <Text
+                    fontSize={14}
+                    lineHeight={"22px"}
+                    mobile={{ fontSize: 12, lineHeight: "18px" }}
+                    fontColor={colors.neutral[400]}
+                  >
                     사업자등록번호 467-34-00514
-                  </Body3>
+                  </Text>
                 </Frame>
-                <Body3 fontColor={colors.neutral[400]}>
+                <Text
+                  fontSize={14}
+                  lineHeight={"22px"}
+                  mobile={{ fontSize: 12, lineHeight: "18px" }}
+                  fontColor={colors.neutral[400]}
+                >
                   서울특별시 서초구 반포대로14길 71 405호
-                </Body3>
-                <Body3 fontColor={colors.neutral[600]}>
+                </Text>
+                <Text
+                  fontSize={14}
+                  lineHeight={"22px"}
+                  mobile={{ fontSize: 12, lineHeight: "18px" }}
+                  fontColor={colors.neutral[600]}
+                >
                   © Hingoray Company. All Right Reserved
-                </Body3>
+                </Text>
               </Frame>
               <Image
                 src="/images/logo/logo.png"
